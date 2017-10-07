e5a73a415c611d751a2bb706188decd362c16009
Your Name <eman_the_man@hotmail.com>
2017-10-07 00:42:14 -0500

    hooked up onsubmit to navigate to room

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public roomName: string;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    alert('hi');
+    this.router.navigate([this.roomName]);
   }
 
 }
