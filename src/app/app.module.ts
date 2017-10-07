ec2a65c6eec346897a0fff939342aa0cfc3d0b9c
Your Name <eman_the_man@hotmail.com>
2017-10-07 00:29:51 -0500

    added materal ui and setup home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { RouterModule, Route } from '@angular/router';
 import { ToastService } from './services/index';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule, MatInputModule, MatCardModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { SimpleToastComponent } from './components/simple-toast/simple-toast.component';
 import { TimerComponent } from './components/timer/timer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleToastComponent,
     TimerComponent,
+    HomeComponent,
   ],
   imports: [
+    MatButtonModule, MatInputModule, MatCardModule,
     BrowserModule,
-    RouterModule.forRoot([<Route>{ path: ":room", component: TimerComponent }])
+    FormsModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      [
+        <Route>{ path: "", component: HomeComponent },
+        <Route>{ path: ":room", component: TimerComponent }
+      ]
+    )
   ],
   providers: [ToastService],
   bootstrap: [AppComponent]
